cc16da95500d92cc343bc2a9899b2f1118ac7b26
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-07-16 15:17:47 +0530

    authentication

---
+++ app/LoginScreen.jsx
@@ -0,0 +1,49 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import {useWarmUpBrowser} from "./hooks/WarmUpBrowser.tsx";
+import { Linking } from 'expo';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginScreen() {
+
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
+
+  return (
+    <View>
+      <Image source={require('../assets/images/login_image.png')} 
+        className="w-[350px] h-[400px] object-cover mx-auto"
+      />
+      <View className="p-8">
+        <Text className="text-[30px] font-bold">Community Marketplace</Text>
+        <Text className="text-[18px] text-slate-500 mt-6">Buy Sell Marketplace where you can sell old item and make real money </Text>
+        <TouchableOpacity 
+          onPress={onPress}
+          className="p-4 bg-blue-500 border border-gray-200 rounded-full mt-20">
+          <Text className='text-white text-center text-[18px]'>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+        </View>
+    </View>
+  )
+}
--- app/index.js
+++ app/index.js
@@ -1,9 +1,31 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import LoginScreen from "./LoginScreen";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-purple-200">
-      <Text className="text-red-500 text-xl">Open up App.js to start working on your app!</Text>
-    </View>
+    <ClerkProvider publishableKey={publishableKey}>
+      <View className="flex-1 items-center justify-center ">
+        <StatusBar style="auto" />
+
+        <SignedIn>
+          <Text> You are signed in</Text>
+        </SignedIn>
+
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+
+      </View>
+    </ClerkProvider>
   );
 }
